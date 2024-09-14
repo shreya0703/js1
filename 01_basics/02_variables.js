1f0e6afae118ca08f455fd8f96785c05b4931815
shreya0703 <147245183+shreya0703@users.noreply.github.com>
2024-09-14 17:09:26 +0000

    ..

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14335
+let accountEmail ="xyz@gmail.com"
+var accountPassword = "12345"
+accountCity = "mumbai"
+let accountState
+
+
+accountEmail ="abs@gmail.com"
+accountPassword="27rkj"
+accountCity="delhi"
+console.table([accountEmail,accountId, accountPassword,accountPassword,accountCity,accountState])
