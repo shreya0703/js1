6496c599fe9a807c6d92673fc830a946f8cf2cb3
shreya0703 <147245183+shreya0703@users.noreply.github.com>
2024-09-14 17:03:58 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,13 @@
+let name = "shreya"
+console.log(typeof name)
+
+let age = 18
+console.log(typeof age)
+let isLoggedIn = false
+console.log(typeof isLoggedIn)
+console.log(typeof null);
+//null is an object
+
+console.log(typeof undefined);
+
+
