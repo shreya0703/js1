806aee9757fefa935d3f60a66e952d2966b20959
shreya0703 <147245183+shreya0703@users.noreply.github.com>
2024-09-14 15:49:58 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,9 +2,10 @@ const accountId = 14335
 let accountEmail ="xyz@gmail.com"
 var accountPassword = "12345"
 accountCity = "mumbai"
+let accountState
 
 
 accountEmail ="abs@gmail.com"
 accountPassword="27rkj"
 accountCity="delhi"
-console.table([accountEmail,accountId, accountPassword,accountPassword,accountCity])
+console.table([accountEmail,accountId, accountPassword,accountPassword,accountCity,accountState])
