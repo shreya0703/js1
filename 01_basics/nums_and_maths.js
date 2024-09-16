7d552975b0feb764ce002d408d3a42b0e808a7f1
shreya0703 <147245183+shreya0703@users.noreply.github.com>
2024-09-16 12:18:22 +0000

    number and maths

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,41 @@
+const score = 500
+//console.log(score);
+//const balance = new Number(100.8584994);
+//console.log(balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));//used frequently 
+
+const num2 = 23.4267
+////console.log(num2.toPrecision(4));//precision
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++     maths      ++++++++++++++++
+
+//console.log(Math);
+
+//console.log(Math.abs(-4));//sign change - to +ve
+//console.log(Math.round(4.2));
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(4.9));
+//console.log(Math.min(4,3,5,7,9));
+//console.log(Math.max(3333333333,444,66));
+
+
+console.log(Math.random())//valu always in between 0 and 1
+console.log(Math.random()*10+1);// to avoid zero value adding 1
+console.log((Math.random()*10)  + 1 );//avoid repitingg values
+console.log(Math.floor(Math.random()*10) + 1 );// to get single nmber
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min)
+//max- min is done to create a range tso that number are chosed between min ad ma and 1 is added so that the answer will not be 2 and lastly +min is added so that the number everytime generateed will be greater than min 
+
+
+
+
+
+
+
