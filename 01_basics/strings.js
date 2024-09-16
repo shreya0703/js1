c6f8870a8aab50e25368efd6ef5454b00da28c54
shreya0703 <147245183+shreya0703@users.noreply.github.com>
2024-09-16 11:35:48 +0000

    strings and their functions

---
+++ 01_basics/strings.js
@@ -0,0 +1,53 @@
+//const name ="shreya-pc"
+//const repoCount = 50
+//console.log(name +  repoCount + " done" );
+//console.log(`hello my name is ${name} and my repoCount is ${repoCount}`);//to print a string
+
+const gameName = new String("shreya-fc-instagram-jio")//object using new keyword 
+//console.log(gameName[0])
+//console.log(gameName.__proto__);//prototype  returns object
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('h'));
+//const newStr = gameName.substring(0,4)//last value is not included
+//console.log(newStr);
+
+//const anotherStr= gameName.slice(-2,4)//in slicing negative value can be given but not in substring
+//console.log(anotherStr);
+
+
+const newStr1 = "       jdjjd        "
+console.log(newStr1);
+console.log(newStr1.trim());// trim removing spaces tarting ending white space and line terimiantors--->/n charaters
+
+
+
+const url ="https://shreya.com/shreya%20raghav"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('shreya'));
+console.log(url.includes('hello'));
+
+
+const split = (gameName.split('-'));
+console.log(split[3]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
