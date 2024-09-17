7c1bed2842e507a35c83087a4ba630caf21f8141
shreya0703 <147245183+shreya0703@users.noreply.github.com>
2024-09-17 17:42:57 +0000

    arrays

---
+++ 02_basics/arrays.js
@@ -0,0 +1,66 @@
+//arrays
+const myArr = [0,1,2,3,4,5,] //jjs have resizable arryas //copy operations creates shallow copies
+//shalow copies--> a shaalow copy is a copy whose properties shares the same  references as those  of the source object from which the copy was made  . As a result , when you change either the source or the copy , you may also cause the other object to change too
+//deep copies---> a deep copy is a copy whose pperties do noy share the same refrences as those of the source object  from which the copy was made. As a result, when you change either the source or copy, you can be assured you are not causing the others objects too
+
+
+//declaring arrays
+
+const Heroes =[ "shaktiman","nagraaj"]
+const myarr2 =new Array(1,2,3,4)
+console.log(myArr[0]);
+
+
+//array methods
+
+//myArr.push(6) // insert values in array
+//myArr.push(7)
+//console.log(myArr);
+//myArr.pop() //deletes last value
+
+//unshift
+//myArr.unshift(9);
+//myArr.shift()
+//console.log(myArr);
+
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(3));
+
+
+//const newArr =  myArr.join()
+//console.log(typeof newArr);
+//console.log(typeof myArr);
+
+
+//slice and spilce
+//slice-----> The slice() method of String values extracts a section of this string and returns it as a new string, without modifying the original string.
+//splice--->The splice() method of Array instances changes the contents of an array by removing or replacing existing elements and/or adding new elements in place
+//console.log("A",myArr);
+////const myn1 = myArr.slice(1,3)
+//console.log(myn1)
+//console.log(myArr);
+
+
+//console.log("B",myArr);
+//console.log(myn1)
+
+//const myn2 = myArr.splice(1,3);
+//console.log(myn2);
+//console.log(myArr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 02_basics/arrays_02.js
@@ -0,0 +1,29 @@
+const Heroes =["thor","ironman","spiderman"]
+const dc =["superman","batman","flash"]
+//push---> only inserts in a already existing array
+//Heroes.push(dc);
+//console.log(Heroes);
+//console.log(Heroes[3][1]);
+
+//concatenation-> can run on new array
+//const all_heroes = Heroes.concat(dc)
+//console.log(all_heroes);
+
+//spread operator
+//const allnew_heroes =[...Heroes,...dc];
+//console.log(allnew_heroes);
+
+
+//const another_array = [1,2,3,4,[4,5],6,[7,8,7,[4,5]]]
+//const real_array = another_array.flat(Infinity)//spreads al the value
+//console.log(real_array);
+
+
+console.log(Array.isArray("hello"))
+console.log(Array.from("hello"))//coverts strrings into array
+console.log(Array.from({name:"shreya"}))//intersting
+
+let score1 = 100
+let score2 = 200
+let score3 =600
+console.log(Array.of(score1,score2,score3));//of is used for number of elements
