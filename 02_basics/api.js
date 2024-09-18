b637f4da3d134937a25154c6b53504c22367c5bf
shreya0703 <147245183+shreya0703@users.noreply.github.com>
2024-09-18 16:24:06 +0000

    JSON

---
+++ 02_basics/api.js
@@ -0,0 +1,25 @@
+//json structure
+
+//     {
+//    "name":"shreya",
+//    "coursename":"js",
+ //   "price":"free"
+//     }
+
+[
+    {},
+    {},
+    {}
+]
+
+//randomeuserme api is famous using its methodon google
+// to study any code of this api to understand it we need to use json formatter
+
+
+// JSON stands for JavaScript Object Notation
+// JSON is a lightweight data interchange format
+// JSON is language independent *
+// JSON is "self-describing" and easy to understand
+// The JSON format is syntactically identical to the code for creating JavaScript objects.
+
+// Because of this similarity, a JavaScript program can easily convert JSON data into native JavaScript objects.
