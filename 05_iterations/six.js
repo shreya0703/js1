8dd3bc8cd834ca2cb501cee221a053584d9c337a
shreya0703 <147245183+shreya0703@users.noreply.github.com>
2024-09-23 16:01:21 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,18 @@
+// const coding =["js","python","cpp","java","ruby"]
+// const values = coding.forEach((items)=>{
+//    // console.log(items);
+//     return items
+    
+
+// })
+// console.log(values); //undefined
+
+
+
+const myNums =[1,3,4,5,6,7,8,9]
+
+// let newNums = myNums.filter((num)=>num>4 )
+// console.log(newNums);
+
+
+
